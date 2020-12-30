14bcb8cba3bca47514a1f6c779621481d3ec3448
Сергей Земсков <nailzed@yandex.ru>
2020-12-30 12:38:19 +0300

    added switching between red yellow and green lights

--- traffic light/ViewController.swift
+++ traffic light/ViewController.swift
@@ -9,7 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var isLightOn = true
+    var changeColor = 1
+    
     
     override var prefersStatusBarHidden: Bool {
         return true
@@ -23,14 +24,24 @@ class ViewController: UIViewController {
 
 
     fileprivate func updateUI() {
-        view.backgroundColor = isLightOn ? .white : .black
-        
+        switch changeColor {
+        case 1:
+            view.backgroundColor = .red
+        case 2:
+            view.backgroundColor = .yellow
+        case 3:
+            view.backgroundColor = .green
+        default:
+            view.backgroundColor = .white
+        }
     }
     
+
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        isLightOn.toggle()
-                updateUI()
+        changeColor += 1
+        if changeColor == 4 {changeColor = 1}
+        updateUI()
     }
 
 }
-
